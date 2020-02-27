0fe175f3683d690a5e28972a7a13feff61ed0324
pankaj Vashisht <iampankajvashisht@gmail.com>
2020-02-27 23:04:31 +0530

    add new key

--- server/config/config.js
+++ server/config/config.js
@@ -6,7 +6,7 @@ const config = {
 	port: process.env.PORT || 4001,
 	root_path: path.resolve(__dirname),
 	GOOGLE_KEY:
-		'AAAAYLkQhLk:APA91bFEbNv-3YOMk0DODV4s2dl2Y6SkKV6Lh49tATwutvKDTBy7bhUN0MNbX3QDmdaNwWJepmwsDBAMBg5HjsWTaf5NvA7Ti29SFMA_xvNJ-mHhrB91taMuK2c7x16TAMrn-a4D0byA'
+		'AAAAggMOQW0:APA91bFj_eW3CEoHulsSClGRnGuJtiJ5hU-4fq_DqDUhZWci2obc2Y89RWgDOfSnyciau0rUdM3GQ3rvoCLskkKnaQ3TRevOPKHFk78_YsA5kiGpa6L0DWi_bYfObUopL-nj-Jkuw9Ng'
 };
 
 module.exports = config;
